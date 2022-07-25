349dcd6b5d07f2b91e674f68dc98e666a328e63e
TolyaTalamanov <anatoliy.talamanov@intel.com>
2022-07-25 22:13:59 +0300

    Support string starts with number

--- tests/test_tokenizer.cpp
+++ tests/test_tokenizer.cpp
@@ -12,6 +12,16 @@ TEST(TokenizerTests, EmptyStream) {
 }
 
 TEST(TokenizerTests, Double) {
+    std::stringstream ss{"42.0"};
+    Tokenizer t(&ss);
+
+    auto tok = t.GetToken();
+
+    EXPECT_TRUE(std::holds_alternative<Tokenizer::Double>(tok));
+    EXPECT_EQ(42, std::get<Tokenizer::Double>(tok).val);
+}
+
+TEST(TokenizerTests, Int) {
     std::stringstream ss{"42"};
     Tokenizer t(&ss);
 
@@ -19,6 +29,49 @@ TEST(TokenizerTests, Double) {
 
     EXPECT_TRUE(std::holds_alternative<Tokenizer::Double>(tok));
     EXPECT_EQ(42, std::get<Tokenizer::Double>(tok).val);
+}
+
+TEST(TokenizerTests, Negative) {
+    std::stringstream ss{"-42.5"};
+    Tokenizer t(&ss);
+
+    auto tok = t.GetToken();
+
+    EXPECT_TRUE(std::holds_alternative<Tokenizer::Double>(tok));
+    EXPECT_EQ(-42.5, std::get<Tokenizer::Double>(tok).val);
+}
+
+TEST(TokenizerTests, StringWithDot) {
+    std::stringstream ss{"42.0.0"};
+    Tokenizer t(&ss);
+
+    auto tok = t.GetToken();
+
+    EXPECT_TRUE(std::holds_alternative<Tokenizer::String>(tok));
+    EXPECT_EQ("42.0.0", std::get<Tokenizer::String>(tok).str);
+}
+
+TEST(TokenizerTests, StringStartsWithNumber) {
+    std::stringstream ss{"1212_foo"};
+    Tokenizer t(&ss);
+
+    EXPECT_EQ("1212_foo", std::get<Tokenizer::String>(t.GetToken()).str);
+}
+
+TEST(TokenizerTests, NumbersWithSlashes) {
+    std::stringstream ss{"0/1/2/"};
+    Tokenizer t(&ss);
+
+    for (int i = 0; i < 3; ++i) {
+        auto tok = t.GetToken();
+        ASSERT_EQ(i, std::get<Tokenizer::Double>(tok).val);
+        t.Next();
+        tok = t.GetToken();
+        ASSERT_TRUE(std::holds_alternative<Tokenizer::Slash>(tok));
+        t.Next();
+    }
+
+    EXPECT_TRUE(t.IsEnd());
 }
 
 TEST(TokenizerTests, String) {
@@ -120,11 +173,4 @@ TEST(TokenizerTests, NextLineBeforeEnd) {
     t.NextLine();
 
     EXPECT_TRUE(t.IsEnd());
-}
-
-TEST(TokenizerTests, ParseStringStartsWithNumber) {
-    std::stringstream ss{"1212_foo"};
-    Tokenizer t(&ss);
-
-    EXPECT_EQ("1212_foo", std::get<Tokenizer::String>(t.GetToken()).str);
 }
